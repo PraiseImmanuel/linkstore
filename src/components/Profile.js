29d5b734102dc7981fe977dd4e9e22c221730a12
PraiseImmanuel <praiseimmanuel601@gmail.com>
2022-10-31 15:44:09 +0100

    Modify Profile Component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,6 +3,14 @@ import "../styles/Profile.css";
 const Profile = () => {
     return (
         <div className="profile">
+            <picture className="share-button">
+                <source
+                    srcSet="./images/share-button.png"
+                    media="(min-width: 768px)"
+                />
+                <source srcSet="./images/share-button-mobile.png" />
+                <img src="./images/share-button-mobile.png" alt="" />
+            </picture>
             <div className="avatar">
                 <div className="pressed">
                     <img src="./images/Icon.png" alt="" />
